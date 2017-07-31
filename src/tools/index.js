b751f84962cea36576e32e7ea00a029b9e943f0a
eric <github@ericjj.com>
2017-07-31 16:51:05 +0800

    编辑器可上传文件

--- src/tools/index.js
+++ src/tools/index.js
@@ -1,5 +1,6 @@
 import moment from 'moment'
 import WangEditor from 'wangeditor'
+import config from '../config'
 
 export default {
   moment: (obj, format = 'YYYY-MM-DD HH:mm:ss') => {
@@ -8,6 +9,41 @@ export default {
   editor: function (vm, onchange) {
     let editor = new WangEditor('#editor')
     editor.customConfig.onchange = onchange
+    editor.customConfig.uploadImgServer = `${config.api}/api/v1/admin/images`
+    editor.customConfig.uploadFileName = 'upload_file'
+    editor.customConfig.uploadImgHooks = {
+      before: function (xhr, editor, files) {
+        // 图片上传之前触发
+        // xhr 是 XMLHttpRequst 对象，editor 是编辑器对象，files 是选择的图片文件
+
+        // 如果返回的结果是 {prevent: true, msg: 'xxxx'} 则表示用户放弃上传
+        // return {
+        //     prevent: true,
+        //     msg: '放弃上传'
+        // }
+      },
+      success: function (xhr, editor, result) {
+        // 图片上传并返回结果，图片插入成功之后触发
+        // xhr 是 XMLHttpRequst 对象，editor 是编辑器对象，result 是服务器端返回的结果
+      },
+      fail: function (xhr, editor, result) {
+        // 图片上传并返回结果，但图片插入错误时触发
+        // xhr 是 XMLHttpRequst 对象，editor 是编辑器对象，result 是服务器端返回的结果
+      },
+      error: function (xhr, editor) {
+        // 图片上传出错时触发
+        // xhr 是 XMLHttpRequst 对象，editor 是编辑器对象
+      },
+      timeout: function (xhr, editor) {
+        // 图片上传超时时触发
+        // xhr 是 XMLHttpRequst 对象，editor 是编辑器对象
+      },
+      customInsert: function (insertImg, result, editor) {
+        const url = result.image.url
+        insertImg(url)
+      }
+    }
+    editor.customConfig.withCredentials = true
     editor.create()
     vm.$store.commit('SET_ITEM', { key: 'htmlEditor', val: editor })
     return editor
