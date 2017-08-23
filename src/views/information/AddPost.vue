e8e2b50fbc738ec30ff7fa56e7abc3b3f0ff8aaf
eric <github@ericjj.com>
2017-08-23 11:38:48 +0800

    发布逻辑修改 // boring

--- src/views/information/AddPost.vue
+++ src/views/information/AddPost.vue
@@ -204,10 +204,8 @@ function updatePost (_this) {
   _this.disabled = true
   api.put(`admin/posts/${_this.$route.query.id}`, _this.form)
   .then((result) => {
-    publish(_this, _this.$route.query.id, () => {
-      _this.$message.success('success')
-      _this.$router.push('/posts')
-    })
+    _this.$message.success('success')
+    _this.$router.push('/posts')
   }).catch((err) => {
     _this.disabled = false
     _this.$message.error(err.toString())
@@ -220,28 +218,26 @@ function createPost (_this) {
   api.post('admin/posts', _this.form)
   .then((result) => {
     console.log(result)
-    publish(_this, result.data.id, () => {
-      _this.$message.success('success')
-      _this.$router.push('/posts')
-    })
+    _this.$message.success('success')
+    _this.$router.push('/posts')
   }).catch((err) => {
     _this.disabled = false
     _this.$message.error(err.toString())
   })
 }
 
-function publish (_this, id, cb) {
-  if (!_this.form.auto_publish_at ||
-    _this.form.auto_publish_at === '' ||
-    _this.form.auto_publish_at === null ||
-    _this.form.auto_publish_at === undefined) {
-    api.patch(`admin/posts/${id}/publish`).then(result => {
-      cb()
-    })
-  } else {
-    cb()
-  }
-}
+// function publish (_this, id, cb) {
+//   if (!_this.form.auto_publish_at ||
+//     _this.form.auto_publish_at === '' ||
+//     _this.form.auto_publish_at === null ||
+//     _this.form.auto_publish_at === undefined) {
+//     api.patch(`admin/posts/${id}/publish`).then(result => {
+//       cb()
+//     })
+//   } else {
+//     cb()
+//   }
+// }
 
 function getPost (_this) {
   api.get(`admin/posts/${_this.$route.query.id}`)
