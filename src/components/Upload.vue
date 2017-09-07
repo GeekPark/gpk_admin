6a5e2815a960c55d835b79661cce75351c3fd518
eric <github@ericjj.com>
2017-09-07 15:21:36 +0800

    upload limit

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -49,9 +49,9 @@ export default {
       this.uploadUrl = response.image.url
     },
     beforeAvatarUpload (file) {
-      const isLt2M = file.size / 1024 / 1024 < 2
+      const isLt2M = file.size / 1024 / 1024 < 20
       if (!isLt2M) {
-        this.$message.error('上传头像图片大小不能超过 2MB!')
+        this.$message.error('上传头像图片大小不能超过 20MB!')
       }
       return isLt2M
     },
