f3c9aa81da99fdbd93c420154de035041ae07c0f
Xinying <corker@126.com>
2019-01-17 16:58:17 +0800

    change topic

--- src/views/information/AddTopic.vue
+++ src/views/information/AddTopic.vue
@@ -11,6 +11,25 @@
       upload(:callback='uploadBanner', :url='form.banner_url', :uploadDelete="deleteBanner")
     el-form-item(label='移动端背景封面（750×350）')
       upload(:callback='uploadAppBanner', :url='form.app_banner_url', :uploadDelete="deleteAppBanner")
+    //- el-form-item(label='栏目模板')
+    //-   el-select(v-model='form.template', placeholder='请选择')
+    //-     el-option(v-for='item in topic_style',
+    //-               :label='item.title',
+    //-               :value='item.val',
+    //-               :key='item.val')
+    //- template(v-show='form.template !== "template_two"')
+    //-   el-form-item(label='banner1')
+    //-     el-input(placeholder='在此输入文章ID', v-model.trim.number='form.banners[0]' clearable)
+    //-   el-form-item(label='banner2')
+    //-     el-input(placeholder='在此输入文章ID', v-model.trim.number='form.banners[1]' clearable)
+    //-   el-form-item(label='banner3')
+    //-     el-input(placeholder='在此输入文章ID', v-model.trim.number='form.banners[2]' clearable)
+    //-   el-form-item(label='banner4')
+    //-     el-input(placeholder='在此输入文章ID', v-model.trim.number='form.banners[3]' clearable)
+    //-   el-form-item(label='banner5')
+    //-     el-input(placeholder='在此输入文章ID', v-model.trim.number='form.banners[4]' clearable)
+    el-form-item(label='是否隐藏标题')
+      el-switch(v-model="form.title_visible", active-text="", inactive-text="")
     el-form-item.actions(label='')
       el-button(type='primary', :disabled='disabled', @click='onSubmit') 提交
       el-button(type='danger', @click='close') 关闭
@@ -29,7 +48,10 @@ export default {
         banner_url: '',
         banner_id: '',
         app_banner_url: '',
-        app_banner_id: ''
+        app_banner_id: '',
+        template: 'template_one',
+        banners: new Array(5),
+        title_visible: false
       },
       rules: {
         title: [
@@ -38,7 +60,14 @@ export default {
         description: [
           { required: true, message: '请输入描述', trigger: 'blur', min: 0 }
         ]
-      }
+      },
+      topic_style: [{
+        title: '正常样式',
+        val: 'template_one'
+      }, {
+        title: '轮播样式',
+        val: 'template_two'
+      }]
     }
   },
   methods: {
@@ -48,6 +77,7 @@ export default {
         if (valid !== true) {
           return this.$message.error('内容信息不完整, 请完善后再提交!')
         } else {
+          // this.form.banner_ids = this.form.banners.filter(item => item)
           if (this.$route.query.id) {
             updateTopic(this)
           } else {
@@ -113,6 +143,16 @@ function createTopic (_this) {
 function getTopic (_this) {
   api.get(`admin/topics/${_this.$route.query.id}`)
   .then((result) => {
+    // const {topic} = result.data
+    // Object.keys(_this.form).forEach(key => {
+    //   _this.form[key] = topic[key] || _this.form[key]
+    // })
+    // if (topic.topic_banner) {
+    //   let arr = new Array(5)
+    //   topic.topic_banner.banners.forEach((item, index) => { arr[index] = item.id })
+    //   _this.form.banners = arr
+    //   _this.form.template = topic.topic_banner.template
+    // }
     _this.form = result.data.topic
   }).catch((err) => {
     _this.$message.error(err.toString())
